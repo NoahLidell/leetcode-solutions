bbf63936fbcaf3ed9b03e3a00383941667f5b4d4
Noah Lidell <noahlidell@gmail.com>
2020-05-02 16:32:00 -0500

    inital copy into browser

---
+++ rust/20_valid_parentheses/valid_parent.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+fn is_valid(s: String) -> bool {
+    let mut stack: Vec<char> = vec![];
+    let parens: HashMap<char, char> = [
+        ("(", ")"), 
+        ("[", "]"), 
+        ("{", "}")
+    ].iter().cloned().collect();
+    for c in s.chars() {
+        if parens.contains_key(&c) {
+            stack.push(c);
+        } else {
+            if let Some(top) = stack.pop() {
+                if c != parens.get(top).unwrap() {
+                    return false
+                }
+            } else {
+                return false
+            }
+        }
+    }
+    stack.is_empty()
+}
