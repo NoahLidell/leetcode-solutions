6d2942970bcb7bf340b0a58917d9f06827f437b3
Noah Lidell <noahlidell@gmail.com>
2020-05-02 16:36:34 -0500

    100 percentile

--- rust/20_valid_parentheses/valid_parent.rs
+++ rust/20_valid_parentheses/valid_parent.rs
@@ -1,17 +1,14 @@
 use std::collections::HashMap;
 fn is_valid(s: String) -> bool {
     let mut stack: Vec<char> = vec![];
-    let parens: HashMap<char, char> = [
-        ("(", ")"), 
-        ("[", "]"), 
-        ("{", "}")
-    ].iter().cloned().collect();
+	let parens: HashMap<char, char> = "([{".chars().zip(")]}".chars()).collect();
     for c in s.chars() {
         if parens.contains_key(&c) {
             stack.push(c);
         } else {
             if let Some(top) = stack.pop() {
-                if c != parens.get(top).unwrap() {
+                // if c != *parens.get(&top).unwrap() { // <- this also compiles
+                if &c != parens.get(&top).unwrap() {
                     return false
                 }
             } else {
