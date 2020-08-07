a242b4717f4059511fa6397143be970bcebbfc1c
Noah Lidell <noahlidell@gmail.com>
2020-08-07 16:58:45 -0500

    dedup

--- python/1525_count_odd_numbers_in_an_interval_range/one_op.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def countOdds(self, low: int, high: int) -> int:
-        below_low = low//2
-        below_high = high//2
-        if high % 2 == 1:
-            below_high += 1
-        return below_high - below_low
