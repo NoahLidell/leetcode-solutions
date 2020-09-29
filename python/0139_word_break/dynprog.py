f47b2808de0d6aef68fe5cda987ff65cdc9e61d3
Noah Lidell <noahlidell@gmail.com>
2020-09-29 17:56:19 -0500

    for any index of the string for each word in dict if thevalue at     index-len(word)-1 is either the beginning or the end of another word     then this index would be marked

---
+++ python/0139_word_break/dynprog.py
@@ -0,0 +1,39 @@
+"""
+139. Word Break
+Medium
+
+Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note:
+
+    The same word in the dictionary may be reused multiple times in the segmentation.
+    You may assume the dictionary does not contain duplicate words.
+
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet", "code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple", "pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+             Note that you are allowed to reuse a dictionary word.
+
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+Output: false
+
+https://leetcode.com/problems/word-break/submissions/
+"""
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        tracker = [0]*len(s)
+        for i in range(len(s)):
+            for w in wordDict:
+                if (s[i-len(w)+1:i+1] == w) and (tracker[i-len(w)] or i-len(w) < 0):
+                    tracker[i] = 1
+        return bool(tracker[-1])
