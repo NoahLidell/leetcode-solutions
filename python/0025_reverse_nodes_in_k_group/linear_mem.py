e5a2458ac1c7df4882726c27c398d3a1ea2d8781
Noah Lidell <noahlidell@gmail.com>
2020-10-08 15:32:21 -0500

    stack based solution, linear memory use

---
+++ python/0025_reverse_nodes_in_k_group/linear_mem.py
@@ -0,0 +1,79 @@
+"""
+25. Reverse Nodes in k-Group
+Hard
+
+Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+Follow up:
+
+    Could you solve the problem in O(1) extra memory space?
+    You may not alter the values in the list's nodes, only nodes itself may be changed.
+
+ 
+
+Example 1:
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [2,1,4,3,5]
+
+Example 2:
+
+Input: head = [1,2,3,4,5], k = 3
+Output: [3,2,1,4,5]
+
+Example 3:
+
+Input: head = [1,2,3,4,5], k = 1
+Output: [1,2,3,4,5]
+
+Example 4:
+
+Input: head = [1], k = 1
+Output: [1]
+
+ 
+
+Constraints:
+
+    The number of nodes in the list is in the range sz.
+    1 <= sz <= 5000
+    0 <= Node.val <= 1000
+    1 <= k <= sz
+    
+https://leetcode.com/problems/reverse-nodes-in-k-group/
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        if not head or k <= 1:
+            return head
+        root = ListNode(0)
+        anchor = root
+        stack = []
+        c = 0
+        while head:
+            nxt = head.next
+            stack.append(head)
+            head = nxt
+            c += 1
+            if c % k == 0:
+                while stack:
+                    node = stack.pop()
+                    anchor.next = node
+                    anchor = anchor.next
+        if stack:
+            anchor.next = stack[0]
+        else:
+            anchor.next = None
+        return root.next
+            
+                
+            
+                
+        
