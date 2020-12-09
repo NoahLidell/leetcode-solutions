7c845ec85d095b1dc3db9d4f91e453996c964b28
Noah Lidell <noahlidell@gmail.com>
2020-12-09 12:37:03 -0600

    brute force all minutes in 12 hrs and count bits

---
+++ python/0401_binary_watch/count_bits.py
@@ -0,0 +1,40 @@
+"""
+401. Binary Watch
+Easy
+
+A binary watch has 4 LEDs on the top which represent the hours (0-11), and the 6 LEDs on the bottom represent the minutes (0-59).
+
+Each LED represents a zero or one, with the least significant bit on the right.
+
+For example, the above binary watch reads "3:25".
+
+Given a non-negative integer n which represents the number of LEDs that are currently on, return all possible times the watch could represent.
+
+Example:
+
+Input: n = 1
+Return: ["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"]
+
+Note:
+
+    The order of output does not matter.
+    The hour must not contain a leading zero, for example "01:00" is not valid, it should be "1:00".
+    The minute must be consist of two digits and may contain a leading zero, for example "10:2" is not valid, it should be "10:02".
+"""
+class Solution:
+    def readBinaryWatch(self, num: int) -> List[str]:
+        def count_bits(n):
+            x = 0
+            while n:
+                if n&1:
+                    x += 1
+                n >>= 1
+            return x
+        times = []
+        for h in range(12):
+            for m in range(60):
+                bits = count_bits(h) + count_bits(m)
+                if bits == num:
+                    times.append(f"{h}:{m:02}")
+        return times
+        
